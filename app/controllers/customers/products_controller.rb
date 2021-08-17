9660b2d2c932ed5c2555296dd1c355f45fc81c4a
ykn113 <yukinosnj13@gmail.com>
2021-08-17 08:35:46 +0000

    [Add]customer product index,show

---
+++ app/controllers/customers/products_controller.rb
@@ -0,0 +1,11 @@
+class Customers::ProductsController < ApplicationController
+  
+  def index
+    @products = Product.all
+  end
+  
+  def show
+    @product = Product.find(params[:id])
+  end
+  
+end
