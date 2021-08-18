0e4eb4b2df90cc86f50624ee48a28a54b0073bf0
keita170 <noinoi0565@gmail.com>
2021-08-18 09:34:56 +0000

    [add]shipping&validates

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -12,5 +12,16 @@ class Customer < ApplicationRecord
   has_many :cart_products, dependent: :destroy
   
   has_many :orders, dependent: :destroy
+  has_many :shipping_addresses, dependent: :destroy
+  
+  
+  validates :last_name, presence: true
+  validates :first_name, presence: true
+  validates :last_name_kana, presence: true
+  validates :first_name_kana, presence: true
+  validates :postcode, presence: true
+  validates :address, presence: true
+  validates :phone_number, presence: true
+  
   
 end
