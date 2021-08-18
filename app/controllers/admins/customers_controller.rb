55514c75abdcb445f54fa4a2f4ace0626d40e7ff
mina2522 <miiina.tb@gmail.com>
2021-08-18 06:27:26 +0000

    [Add] admin customers show

--- app/controllers/admins/customers_controller.rb
+++ app/controllers/admins/customers_controller.rb
@@ -1,6 +1,10 @@
 class Admins::CustomersController < ApplicationController
   def index
     @customers = Customer.page(params[:page]).reverse_order
-    
   end
+
+  def show
+    @customer = Customer.find(params[:id])
+  end
+
 end
