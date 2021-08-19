3848de4e27c4d08bf5fe19101b5a6f608058d623
mina2522 <miiina.tb@gmail.com>
2021-08-19 10:37:46 +0000

    [Add] admin orders show

--- app/controllers/admins/orders_controller.rb
+++ app/controllers/admins/orders_controller.rb
@@ -1,2 +1,17 @@
 class Admins::OrdersController < ApplicationController
+  def show
+    @order = Order.find(params[:id])
+  end
+  
+  def update
+    @order = Order.find(params[:id])
+    @order.update(order_params)
+    redirect_to admins_order_path(@order.id)
+  end
+  
+  private
+  
+  def order_params
+    params.require(:order).permit(:status)  
+  end
 end
--- app/models/order_product.rb
+++ app/models/order_product.rb
@@ -1,4 +1,11 @@
 class OrderProduct < ApplicationRecord
     belongs_to :order
     belongs_to :product
+    
+    enum status: {
+        cannot_be_started: 0,
+        waiting: 1,
+        making: 2,
+        conpleted: 3
+    }
 end
