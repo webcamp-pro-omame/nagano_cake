ac718ebfdf7da06cfd598f3723fc58fe5656e065
mina2522 <miiina.tb@gmail.com>
2021-08-19 06:28:41 +0000

    [Add] admin orders index

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,13 @@
 class Order < ApplicationRecord
-    
+
     belongs_to :customer
-    has_many : order_products, dependent: :destroy
+    has_many :order_products, dependent: :destroy
+    
+    enum status: {
+        waiting: 0,
+        paid_up: 1,
+        making: 2,
+        preparing: 3,
+        shipped: 4
+    }
 end
--- config/application.rb
+++ config/application.rb
@@ -25,8 +25,8 @@ module NaganoCake
      #　#　以下の記述を追記する(設定必須)
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
     
-    
-    
+    config.time_zone = 'Asia/Tokyo'
+    config.i18n.default_locale = :ja
   end
   
   
