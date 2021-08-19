94511e354c444c8c06b84fd37c66fa3a1d25de62
ykn113 <yukinosnj13@gmail.com>
2021-08-19 10:15:58 +0000

    [Update]cart_product index

--- app/controllers/customers/cart_products_controller.rb
+++ app/controllers/customers/cart_products_controller.rb
@@ -1,36 +1,36 @@
 class Customers::CartProductsController < ApplicationController
-  
+
   def index
-    @cart_products = current.customer.cart_products
+    @cart_products = CartProduct.all
   end
-  
+
   def update
     @cart_product = CartProduct.find(params[:id])
-    @cart_product.update(amount: params[:amount].to_i)
+    @cart_product.update(cart_product_params)
     redirect_to cart_products_path
   end
-  
+
   def destroy
     @cart_product = CartProduct.find(params[:id])
     @cart_product.destroy
     redirect_to cart_products_path
   end
-  
+
   def destroy_all
-    @cart_products = current.customer.cart_products
+    @cart_products = CartProduct.all
     @cart_products.destroy_all
     redirect_to cart_products_path
   end
-  
+
   def create
-    @cart_product = current_user.cart_items.new(cart_product_params)
+    @cart_product = CartProduct.new(cart_product_params)
     @cart_product.save
     redirect_to cart_products_path
   end
-  
+
   private
   def cart_product_params
     params.require(:cart_product).permit(:product_id, :customer_id, :amount)
   end
-  
+
 end
