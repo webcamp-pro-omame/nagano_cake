933bc2ab5e23c8e89e5b20c1d7f347e2ea6e03de
mina2522 <miiina.tb@gmail.com>
2021-08-22 09:54:33 +0000

    [Update] adminのorderのindex,showページ

--- app/controllers/admins/homes_controller.rb
+++ app/controllers/admins/homes_controller.rb
@@ -1,5 +1,5 @@
 class Admins::HomesController < ApplicationController
   def top
-    @orders = Order.all.page(params[:page]).per(10)
+    @orders = Order.all.page(params[:page]).per(10).order(created_at: :desc)
   end
 end
