9aba3e7cf13a19215e5c0202223ffbe481b7eb02
keita170 <noinoi0565@gmail.com>
2021-08-27 03:18:22 +0000

    customer_comment

--- app/controllers/customers/orders_controller.rb
+++ app/controllers/customers/orders_controller.rb
@@ -1,7 +1,8 @@
 class Customers::OrdersController < ApplicationController
-    require "payjp"
+
     
     def index
+        #最新の注文から並べるために.reverse_orderを使用している
         @orders = current_customer.orders.page(params[:page]).per(8).reverse_order
     end
     
@@ -17,32 +18,21 @@ class Customers::OrdersController < ApplicationController
     end
     
     
-    
+    #newページから情報を取得して、confirmページ上に情報を書き出すための記述
     def confirm
         @cart_products = current_customer.cart_products
         @order = Order.new(order_params)
         @order.shipping_fee = 800
-        
-        #@order.bill = current_customer.cart_products.total_price
         @order.payment_method = params[:order][:payment_method]
-        #if @order.payment_method == "クレジットカード"
-         # Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
-        #  charge = Payjp::Charge.create(
-         # amount: params[:order][:bill], # 決済する値段
-         # card: params['payjp-token'], # フォームを送信すると作成・送信されてくるトークン
-         # currency: 'jpy'
-         # )
-        #end
         
+        #ラジオボタンで選択した時の取得する情報の条件分岐、enumではなくnew.html.erbで0,1,2を振り分けている
         if params[:order][:address_num] == "0"
             @order.postcode = current_customer.postcode
             @order.address = current_customer.address
             @order.name = current_customer.last_name + current_customer.first_name
         elsif params[:order][:address_num] == "1"
-            #@customer = current_customer
-            @cart_products = current_customer.cart_products
+            #f.selectのaddress_boxの中の/:id の番号を取得する
             @shipping_address = ShippingAddress.find(params[:order][:address_box])
-            
             @order.postcode = @shipping_address.postcode
             @order.address = @shipping_address.address
             @order.name = @shipping_address.name
@@ -52,7 +42,7 @@ class Customers::OrdersController < ApplicationController
             @order.address = params[:order][:address]
             @order.name = params[:order][:name]
         end
-        #byebug
+        
     end
     
     def show
@@ -60,21 +50,15 @@ class Customers::OrdersController < ApplicationController
         @order_products = @order.order_products
     end
     
-    
-    
-    
-    
-    
+    #confirmページで注文を確定するを押した後の動作を記述してる
     def create
-        
+        #order_paramsで取得できる情報の保存
         order = Order.new(order_params)
         order.status = 0
         order.customer_id = current_customer.id
         order.save
         
-        
-        
-        #byebug
+        #商品情報の保存
         cart_products = current_customer.cart_products
         cart_products.each do |item|
             order_product = OrderProduct.new
@@ -85,10 +69,9 @@ class Customers::OrdersController < ApplicationController
             order_product.status = 0
             order_product.save
         end
-        
-        
-        
+
         current_customer.cart_products.destroy_all
+        
         
         unless Customer.where(id: order.customer_id).where(postcode: order.postcode).where(address: order.address) || ShippingAddress.where(customer_id: order.customer_id).where(name: order.name).where(postcode: order.postcode).where(address: order.address).exits?                                       
             shipping_address = ShippingAddress.new
@@ -98,21 +81,13 @@ class Customers::OrdersController < ApplicationController
             shipping_address.address = order.address
             shipping_address.save
         end
-        
-        
         redirect_to orders_thanks_path
     end
-    
     
     private
     
     def order_params
         params.require(:order).permit(:payment_method, :postcode, :address, :name, :bill, :shipping_fee, :status, :customer_id)
     end
-    
-    #def order_params
-    #    params.permit(:payment_method, :postcode, :address, :name, :bill, :shipping_fee, :customer_id, :status)
-    #end
-    
     
 end
